d5b69caef07a5debd6e19af456240822dbad2395
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-04-19 10:50:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Part_6__Random_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
